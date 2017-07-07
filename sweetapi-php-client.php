7bb8085d3a3a1c0bd3ca464adf58affafd3e24b0
ıqO <merenuou@gmail.com>
2017-07-07 02:04:20 -0600

    remove unnneeded cURL commands and adjust file comments.

--- sweetapi-php-client.php
+++ sweetapi-php-client.php
@@ -1,25 +1,5 @@
 <?php
 
-// $ch = curl_init('http://localhost:8080/stocks/add');
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-//     'Content-Type: application/json',
-//     'Content-Length: ' . strlen($data_string))
-// );
-// curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-// curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-//
-// //execute post
-// $result = curl_exec($ch);
-//
-// //close connection
-// curl_close($ch);
-//
-// echo $result;
-
-
 // GET http://swiq3.com:8080/locations?apikey=YOUR_API_KEY&client_id=YOUR_CLIENT_ID
 
 class sweetClient {
@@ -28,11 +8,20 @@ class sweetClient {
   public $response_object;
   public $response_info;
 
+   /**
+   * Constructor.
+   *
+   */
   function __construct() {
     $this->http_options = [];
+    
+// @todo 
+// curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+// curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
     $this->http_options[CURLOPT_RETURNTRANSFER] = true;
     $this->http_options[CURLOPT_FOLLOWLOCATION] = false;
   }
+  
   /**
    * Perform a GET call to server
    *
@@ -44,95 +33,26 @@ class sweetClient {
    * @param array $http_options Extra option to pass to curl handle.
    * @return string The response from curl if any
    */
-  function get($url, $http_options = array()) {
+  public function get($url, $http_options = array()) {
     $http_options = $http_options + $this->http_options;
     $this->handle = curl_init($url);
     if(! curl_setopt_array($this->handle, $http_options)){
       throw new RestClientException("Error setting cURL request options");
     }
     $this->response_object = curl_exec($this->handle);
-    $this->http_parse_message($this->response_object);
+    $this->_http_parse_message($this->response_object);
     curl_close($this->handle);
     return $this->response_object;
   }
+ 
   /**
-   * Perform a POST call to the server
+   * Response handler.
    *
-   * Additionaly in $response_object and $response_info are the
-   * response from server and the response info as it is returned
-   * by curl_exec() and curl_getinfo() respectively.
    *
-   * @param string $url The url to make the call to.
-   * @param string|array The data to post. Pass an array to make a http form post.
-   * @param array $http_options Extra option to pass to curl handle.
-   * @return string The response from curl if any
+   * @param string $res the response object from the API.
+   * @return string exception
    */
-  function post($url, $fields = array(), $http_options = array()) {
-    $http_options = $http_options + $this->http_options;
-    $http_options[CURLOPT_POST] = true;
-    $http_options[CURLOPT_POSTFIELDS] = $fields;
-    if(is_array($fields)){
-      $http_options[CURLOPT_HTTPHEADER] =
-        array('Content-Type: multipart/form-data');
-    }
-    $this->handle = curl_init($url);
-    if(! curl_setopt_array($this->handle, $http_options)){
-      throw new RestClientException("Error setting cURL request options.");
-    }
-    $this->response_object = curl_exec($this->handle);
-    $this->http_parse_message($this->response_object);
-    curl_close($this->handle);
-    return $this->response_object;
-  }
-  /**
-   * Perform a PUT call to the server
-   *
-   * Additionaly in $response_object and $response_info are the
-   * response from server and the response info as it is returned
-   * by curl_exec() and curl_getinfo() respectively.
-   *
-   * @param string $url The url to make the call to.
-   * @param string|array The data to post.
-   * @param array $http_options Extra option to pass to curl handle.
-   * @return string The response from curl if any
-   */
-  function put($url, $data = '', $http_options = array()) {
-    $http_options = $http_options + $this->http_options;
-    $http_options[CURLOPT_CUSTOMREQUEST] = 'PUT';
-    $http_options[CURLOPT_POSTFIELDS] = $data;
-    $this->handle = curl_init($url);
-    if(! curl_setopt_array($this->handle, $http_options)){
-      throw new RestClientException("Error setting cURL request options.");
-    }
-    $this->response_object = curl_exec($this->handle);
-    $this->http_parse_message($this->response_object);
-    curl_close($this->handle);
-    return $this->response_object;
-  }
-  /**
-   * Perform a DELETE call to server
-   *
-   * Additionaly in $response_object and $response_info are the
-   * response from server and the response info as it is returned
-   * by curl_exec() and curl_getinfo() respectively.
-   *
-   * @param string $url The url to make the call to.
-   * @param array $http_options Extra option to pass to curl handle.
-   * @return string The response from curl if any
-   */
-  function delete($url, $http_options = array()) {
-    $http_options = $http_options + $this->http_options;
-    $http_options[CURLOPT_CUSTOMREQUEST] = 'DELETE';
-    $this->handle = curl_init($url);
-    if(! curl_setopt_array($this->handle, $http_options)){
-      throw new RestClientException("Error setting cURL request options.");
-    }
-    $this->response_object = curl_exec($this->handle);
-    $this->http_parse_message($this->response_object);
-    curl_close($this->handle);
-    return $this->response_object;
-  }
-  private function http_parse_message($res) {
+  private function _http_parse_message($res) {
     if(! $res){
       throw new HttpServerException(curl_error($this->handle), -1);
     }
